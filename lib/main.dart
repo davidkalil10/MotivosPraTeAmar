262a75ccad69cac8747cac026e159ed513576682
David Kalil <dmmd10@hotmail.com>
2023-01-24 15:46:21 -0300

    firebase configurado

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:motivosprateamar/Home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     title: "Motivos Pra Te Amar",
     home: Home(),
