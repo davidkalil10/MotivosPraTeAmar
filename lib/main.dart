9ddd2e4db1713dae1fdcd5904755d338b9198e8f
David Kalil <dmmd10@hotmail.com>
2023-02-07 15:11:14 -0300

    implementando web view

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:motivosprateamar/Home.dart';
+import 'package:motivosprateamar/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MaterialApp(
     title: "Motivos Pra Te Amar",
     home: Home(),
